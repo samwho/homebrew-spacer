9452b31cce61b3db5da95d9fde66f58c43d6777d
Sam Rose <hello@samwho.dev>
2023-06-25 17:29:52 +0100

    v0.1.8

--- Formula/spacer.rb
+++ Formula/spacer.rb
@@ -1,14 +1,14 @@
 class Spacer < Formula
-  version '0.1.6'
+  version '0.1.8'
   desc "A small command-line utility for adding spacers to command output"
   homepage "https://github.com/samwho/spacer"
 
   if OS.mac?
       url "https://github.com/samwho/spacer/releases/download/v#{version}/spacer-x86_64-apple-darwin.tar.gz"
-      sha256 "6a09295bb1b06349702d6beeb2c0521a7cfd135d4105c91ed43fff690a6bee4c"
+      sha256 "c244f98faa581105051278827705cbb3e6c6d40d6d956d9aa483fcefe11b4b57"
   elsif OS.linux?
-      url "https://github.com/samwho/spacer/releases/download/v#{version}/spacer-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "1f2c04950920996c826bdfa1de543ccd117fec784663bb9cb733924ac30e9518"
+      url "https://github.com/samwho/spacer/releases/download/v#{version}/spacer-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "c127514d126bb369003ccbce72a4d147c9447ff0bf055e37c858fac6b92dd34a"
   end
 
   def install
