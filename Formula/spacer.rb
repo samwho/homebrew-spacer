9ca02af299bbc5ee81d96c01c058ba6d5ce2909c
Sam Rose <hello@samwho.dev>
2023-08-31 21:12:10 +0100

    Fix syntax error.

--- Formula/spacer.rb
+++ Formula/spacer.rb
@@ -8,9 +8,10 @@ class Spacer < Formula
     if OS.mac?
       url "https://github.com/samwho/spacer/releases/download/v#{version}/spacer-x86_64-apple-darwin.tar.gz"
       sha256 "afb048048a420a48d325141ab1c447f871ab6dacb04143f198c7ecfc640e7e0e"
-  elsif OS.linux?
-      url "https://github.com/samwho/spacer/releases/download/v#{version}/spacer-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "1bdb7debf6387bbe6da68ee70c8edbcac7f8affa43665f82e391212bba610985"
+    elsif OS.linux?
+        url "https://github.com/samwho/spacer/releases/download/v#{version}/spacer-x86_64-unknown-linux-musl.tar.gz"
+        sha256 "1bdb7debf6387bbe6da68ee70c8edbcac7f8affa43665f82e391212bba610985"
+    end
   end
 
   on_arm do
